d7fe63b0efcf8a3288aa4ae264410670ee309e7a
Elvis Ciotti <elvisciotti@gmail.com>
2018-06-11 17:57:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# DigiCop
+
+
+## Install and run (docker)
+
+    docker-compose build
+    docker-compose up
+
+## Useful commands
+
+    # bash into container
+    docker exec -it dc_frontend bash
+    # watch logs
+    docker exec -it dc_frontend bash -c "tail -f /var/log/app/*"
+    
+
+## VIEW
+
+[https://localhost/](https://localhost/) (confirm security exception)
---
+++ frontend/src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        // deputy homepage with links to register and login
+        return $this->render('AppBundle:Index:index.html.twig');
+    }
+
+}
