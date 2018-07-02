d5ca12caf0a6b7a05493a4772640ce38eed0a00e
Biggs Thorarensen <biggs@devillion.com>
2018-07-02 18:00:43 +0100

    DDPB-2214 govuk frontend installed. sass processed into web/assets directory. cleaned out and rebuilt on build. layout resolving path with assetUrl filter (borrowed from Digideps)

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,8 +1,35 @@
 'use strict';
 
-var gulp = require('gulp')
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    clean = require('gulp-clean'),
+    now = new Date().getTime();
+
+var config = {
+    sass: {
+        includePaths: [
+            'node_modules/govuk-frontend'
+        ]
+    },
+    sassSrc: 'src/AppBundle/Resources/assets/scss',
+    webAssets: 'web/assets/' + now,
+}
+
+// Clean out old assets
+gulp.task('clean', function () {
+    return gulp.src('web/assets/*', {read: false})
+        .pipe(clean());
+});
+
+// Compile sass files
+gulp.task('sass', ['clean'], function () {
+    return gulp.src([
+            config.sassSrc + '/application.scss'])
+        .pipe(sass(config.sass).on('error', sass.logError))
+        .pipe(gulp.dest(config.webAssets + '/stylesheets'));
+});
 
 // Basics
-gulp.task('default', function () {
-    console.log('Gulp up and running');
+gulp.task('default', ['sass'], function () {
+    console.log('Building assets');
 });
