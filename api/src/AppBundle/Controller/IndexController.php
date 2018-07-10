0f6d85e94df2f5c0c15df29445113e53cb8bfe90
Elvis Ciotti <elvisciotti@gmail.com>
2018-07-10 12:44:33 +0200

    DDPB-2221-api tmp

---
+++ api/src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+
+}
