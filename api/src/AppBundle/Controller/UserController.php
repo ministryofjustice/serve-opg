423309d0fe8c51ac612ee9bc5a8de6bed0afff00
Elvis Ciotti <elvisciotti@gmail.com>
2018-07-23 12:35:31 +0200

    doctrine user sample routes

--- api/src/AppBundle/Controller/UserController.php
+++ api/src/AppBundle/Controller/UserController.php
@@ -2,13 +2,36 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * @Route("/user")
+ */
 class UserController extends Controller
 {
+    /**
+     * @Route("/all", name="user_all")
+     */
+    public function all()
+    {
+        return $this->get('em')->getRepository(User::class)->findAll();
+    }
 
+    /**
+     * @Route("/by-id/{id}", name="user_by_id", requirements={"id":"\d+"})
+     */
+    public function oneById($id)
+    {
+        return $this->get('em')->getRepository(User::class)->findOneBy(['id'=>$id]);
+    }
+
+    /**
+     * @Route("/by-email/{email}", name="user_by_email", requirements={"email":"\w+"})
+     */
+    public function oneByEmail($email)
+    {
+        return $this->get('em')->getRepository(User::class)->findOneBy(['email'=>$email]);
+    }
 }
