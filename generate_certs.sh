a7fe1dcdafa33728129cf55ee982165ef495fa04
Luke Bradbury <luke@nqyr.io>
2018-09-06 18:15:49 +0100

    Drop unnecessary SAN support

--- generate_certs.sh
+++ generate_certs.sh
@@ -7,10 +7,6 @@ openssl req \
     -new \
     -out certs/web.crt \
     -subj /CN=\localhost \
-    -reqexts SAN \
-    -extensions SAN \
-    -config <(cat /System/Library/OpenSSL/openssl.cnf \
-        <(printf '[SAN]\nsubjectAltName=DNS:localhost')) \
     -sha256 \
     -days 3650
 
