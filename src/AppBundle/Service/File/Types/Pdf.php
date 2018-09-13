c2a1bd5cc5a17f4ce624d7cadb61502968320a73
Shaun Lizzio <shaun@lizzio.co.uk>
2018-09-13 09:20:46 +0100

    DCOP-22 removed method, doing same as parent

--- src/AppBundle/Service/File/Types/Pdf.php
+++ src/AppBundle/Service/File/Types/Pdf.php
@@ -5,14 +5,4 @@ namespace AppBundle\Service\File\Types;
 class Pdf extends UploadableFile
 {
     protected $scannerEndpoint = 'upload/pdf';
-
-    /**
-     * Checks a file by calling configured file checkers for that file type
-     *
-     * @throws \Exception
-     */
-    public function checkFile()
-    {
-        parent::callFileCheckers();
-    }
 }
