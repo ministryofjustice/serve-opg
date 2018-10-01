4b8b293c940cce35a030defbf4d24362789dfa12
Shaun Lizzio <shaun@lizzio.co.uk>
2018-10-01 15:49:45 +0100

    DCOP-62 Generated API JSON payload

--- src/AppBundle/Service/SiriusService.php
+++ src/AppBundle/Service/SiriusService.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Entity\Client;
+use AppBundle\Entity\Deputy;
+use AppBundle\Entity\Document;
 use AppBundle\Entity\Order;
 use Application\Factory\GuzzleClient;
 use Aws\CommandPool;
@@ -54,27 +57,33 @@ class SiriusService
 
         try {
 
-            $documents = $this->sendDocuments($order->getDocuments());
-
-            // persist documents with new location added
-            foreach ($documents as $document) {
-                $this->em->persist($document);
-            }
-            $this->em->flush();
+//            $documents = $this->sendDocuments($order->getDocuments());
+//
+//            // persist documents with new location added
+//            foreach ($documents as $document) {
+//                $this->em->persist($document);
+//            }
+//            $this->em->flush();
 
             // Begin API call to Sirius
             // login to establish connectivity
             //$return = $this->login();
 
             // generate JSON payload of order
-            //$payload = $this->generatePayload($order);
+            $payload = $this->generateOrderPayload($order);
 
-            // Make API call
-            //$return = $this->httpClient->serveOrderToSirius($payload);
+            if ($payload) {
+                echo $payload;
+                exit;
+                // Make API call
+                //$return = $this->httpClient->serveOrderToSirius($payload);
+            }
+            $return = $this->logout();
 
         } catch (\Exception $e) {
             throw $e;
         }
+        exit;
     }
 
     /**
@@ -91,32 +100,214 @@ class SiriusService
         return $documents;
     }
 
-//    private function login()
-//    {
-//        $jar = new \GuzzleHttp\Cookie\CookieJar;
-//
-//        try {
-//            $response = $this->httpClient->request(
-//                'POST',
-//                'https://localhost:8080/auth/login',
-//                [
-//                        'email' => 'manager@opgtest.com',
-//                        'password' => 'Password1',
-////                        'cookies' => $jar
-//                ]
-//            );
-//            echo 'RESPONSE--> ';
-//            var_dump($response);exit;
-//        } catch (RequestException $e) {
-//            echo Psr7\str($e->getRequest());
-//            echo '--------------<br />';
-//            var_dump($e->getCode());
-//
-//            var_dump($e->getMessage());
-//            if ($e->hasResponse()) {
-//                echo Psr7\str($e->getResponse());
-//            }
-//        }
-//
-//    }
+    /**
+     * Generates JSON payload for Sirius API call
+     *
+     * @param Order $order
+     * @return string
+     */
+    private function generateOrderPayload(Order $order)
+    {
+        return json_encode(
+            [
+                $this->generateOrderDetails($order),
+                'client' => $this->generateClientDetails($order->getClient()),
+                'deputies' => $this->generateDeputiesDetails($order->getDeputies()),
+                'documents' => $this->generateDocumentDetails($order->getDocuments())
+            ]
+        );
+    }
+
+    /**
+     * Generates Order details for Sirius API call
+     *
+     * @param Order $order
+     * @return array
+     */
+    private function generateOrderDetails(Order $order)
+    {
+        return [
+            "courtReference" => $order->getClient()->getCaseNumber(),
+            "type" => $order->getType() == 'hw' ? 'HW' : 'PF',
+            "subType" => $order->getSubType(),
+            "date" => $order->getCreatedAt()->format('Y-m-d'),
+            "issueDate" => $order->getIssuedAt()->format('Y-m-d'),
+            "appointmentType" => $order->getAppointmentType(),
+            "assetLevel" => $order->getHasAssetsAboveThreshold() ? 'HIGH' : 'LOW',
+        ];
+    }
+
+    /**
+     * Generates client details as array in preparation for Sirius API call
+     *
+     * @param Client $client
+     * @return array
+     */
+    private function generateClientDetails(Client $client)
+    {
+        return [
+            "firstName" => self::extractFirstname($client->getClientName()),
+            "lastName" => self::extractLastname($client->getClientName())
+        ];
+    }
+
+    /**
+     * Generates an array of deputy arrays for API call to Sirius
+     *
+     * @param ArrayCollection $deputies
+     * @return array
+     */
+    private function generateDeputiesDetails(Collection $deputies)
+    {
+        $deputyArray = [];
+        /** @var Deputy $deputy */
+        foreach ($deputies as $deputy) {
+            $deputyArray[] = $this->generateDeputyArray($deputy);
+        }
+
+        return $deputyArray;
+    }
+
+    /**
+     * Generates data array for a single deputy
+     *
+     * @param Deputy $deputy
+     * @return array
+     */
+    private function generateDeputyArray(Deputy $deputy)
+    {
+        return [
+            "type" => $deputy->getDeputyType(),
+            "firstName" => $deputy->getForename(),
+            "lastName" => $deputy->getSurname(),
+            "dob" => $deputy->getDateOfBirth()->format('Y-m-d'),
+            "email" => $deputy->getEmailAddress(),
+            "daytimeNumber" => $deputy->getDaytimeContactNumber(),
+            "eveningNumber" => $deputy->getEveningContactNumber(),
+            "mobileNumber" => $deputy->getMobileContactNumber() ,
+            "addressLine1" => $deputy->getAddressLine1(),
+            "addressLine2" => $deputy->getAddressLine2(),
+            "addressLine3" => $deputy->getAddressLine3(),
+            "town" => $deputy->getAddressTown(),
+            "county" => $deputy->getAddressCounty(),
+            "postcode" => $deputy->getAddressPostcode()
+        ];
+    }
+
+    /**
+     * Extract first name from a full name string
+     *
+     * @param string $fullName
+     * @return mixed
+     */
+    protected static function extractFirstname($fullName)
+    {
+        $name = explode(' ', $fullName, 2);
+        return implode(' ', array_slice($name, 0, -1));
+    }
+
+
+    /**
+     * Extract first name from a full name string
+     *
+     * @param string $fullName
+     * @return mixed
+     */
+    protected static function extractLastname($fullName)
+    {
+        $name = explode(' ', $fullName, 2);
+        return implode(' ', array_slice($name, 1));
+    }
+
+    /**
+     * Generates an array of document arrays for API call to Sirius
+     *
+     * @param ArrayCollection $documents
+     *
+     * @return array
+     */
+    private function generateDocumentDetails(   Collection $documents)
+    {
+        $docsArray = [];
+        /** @var Document $doc */
+        foreach ($documents as $doc) {
+            $docsArray[] = $this->generateDocumentArray($doc);
+        }
+
+        return $docsArray;
+    }
+
+    /**
+     * Generates data array for a single document
+     *
+     * @param Document $document
+     *
+     * @return array
+     */
+    private function generateDocumentArray(Document $document)
+    {
+        return [
+            "type" => $document->getType(),
+            "s3Link" => $document->getStorageReference()
+        ];
+    }
+
+    /**
+     * Login to Sirius
+     */
+    private function login()
+    {
+        $jar = new \GuzzleHttp\Cookie\CookieJar;
+
+        try {
+            $response = $this->httpClient->request(
+                'POST',
+                'https://frontend-datamigration.dev.sirius.opg.digital',
+                [
+                    'form_params' => [
+                        'email'     => 'manager@opgtest.com',
+                        'password' => 'Password1',
+                        'cookies' => $jar
+                    ]
+                ]
+            );
+            echo 'RESPONSE--> ';
+            var_dump($response);exit;
+        } catch (RequestException $e) {
+            echo Psr7\str($e->getRequest());
+            echo '--------------<br />';
+            var_dump($e->getCode());
+
+            var_dump($e->getMessage());
+            if ($e->hasResponse()) {
+                echo Psr7\str($e->getResponse());
+            }
+        }
+
+    }
+
+    private function logout()
+    {
+        try {
+            $response = $this->httpClient->request(
+                'POST',
+                'https://frontend-datamigration.dev.sirius.opg.digital/auth/logout',
+                [
+                    'form_params' => []
+                ]
+            );
+            echo 'RESPONSE--> ';
+            var_dump($response);exit;
+        } catch (RequestException $e) {
+            echo Psr7\str($e->getRequest());
+            echo '--------------<br />';
+            var_dump($e->getCode());
+
+            var_dump($e->getMessage());
+            if ($e->hasResponse()) {
+                echo Psr7\str($e->getResponse());
+            }
+        }
+
+    }
 }
