3052bc434be1124900fe00a9e0bd0b9131f42682
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2018-10-08 14:46:59 +0200

    DCOP-61-login-protection move table creationg to service

--- src/Common/SessionConnectionCreatingTable.php
+++ src/Common/SessionConnectionCreatingTable.php
@@ -38,35 +38,18 @@ class SessionConnectionCreatingTable extends StandardSessionConnection
 {
     public function __construct(DynamoDbClient $client, array $config = [])
     {
+        if (empty($config['table_name'])) {
+            throw new \InvalidArgumentException(__METHOD__.': table_name missing');
+        }
         $tableName = $config['table_name'];
+        if (empty($config['hash_key'])) {
+            throw new \InvalidArgumentException(__METHOD__.': hash_key missing');
+        }
+        $hashKey = $config['hash_key'];
 
         parent::__construct($client, $config = []);
 
-        // enable the following to delete the table, for testing purposes only
-        //$client->deleteTable(['TableName' => $tableName]);
-
-        // create table on the fly if not existing
-        if (!in_array($tableName, $client->listTables()['TableNames'])) {
-            $params = [
-                'TableName' => $tableName,
-                'KeySchema' => [
-                    [
-                        'AttributeName' => 'id',
-                        'KeyType' => 'HASH',  //Partition key
-                    ]
-                ],
-                'AttributeDefinitions' => [
-                    [
-                        'AttributeName' => 'id',
-                        'AttributeType' => 'S'
-                    ]
-                ],
-                'ProvisionedThroughput' => [
-                    'ReadCapacityUnits' => 10,
-                    'WriteCapacityUnits' => 10
-                ]
-            ];
-            $client->createTable($params);
-        }
+        $dtc = new DynamoDbUtilities($client);
+        $dtc->createHashTableIfNotExisting($tableName, $hashKey);
     }
 }
