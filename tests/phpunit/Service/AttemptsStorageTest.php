faf56dbd4915139f33f1d22427297c43cd1b300b
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2018-10-09 15:01:27 +0200

    DCOP-61-login-protection hastoWait test

--- tests/phpunit/Service/AttemptsStorageTest.php
+++ tests/phpunit/Service/AttemptsStorageTest.php
@@ -9,7 +9,19 @@ class AttemptsStorageTest extends \PHPUnit_Framework_TestCase
     public static function hasToWaitProvider()
     {
         return [
+            // no rules => never locked
             [[], 5, 60, 600, 100, false],
+            // one attempt => never locked
+            [[0], 0, 60, 600, 0, false],
+            [[0], 1, 60, 600, 0, false],
+            // 2 attempts reached in last 500 seconds at time 1500 => max was 3. not locked
+            [[0, 100, 1200, 1300], 3, 500, 5000, 1500, false],
+            // 3 attempts reached in last 500 seconds at time 1500 => locked for 5000 seconds
+            [[0, 100, 1200, 1300, 1500], 3, 500, 5000, 1500, 5000],
+            // after 500 seconds, still locked for 4500 seconds
+            [[0, 100, 1200, 1300, 1500], 3, 500, 5000, 2000, 4500],
+            // after 5001 seconds, unlocked
+            [[0, 100, 1200, 1300, 1500], 3, 500, 5000, 2000, 4500],
         ];
     }
 
