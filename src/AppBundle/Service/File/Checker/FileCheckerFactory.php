0eda8f26df0be1deaa470b260e8de08f3ecc0196
Shaun Lizzio <shaun@lizzio.co.uk>
2018-10-22 14:25:44 +0100

    DCOP-87 Update FileCheckerFactory to create new file objects

--- src/AppBundle/Service/File/Checker/FileCheckerFactory.php
+++ src/AppBundle/Service/File/Checker/FileCheckerFactory.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Service\File\Checker;
 
+use AppBundle\Service\File\Checker\Exception\InvalidFileTypeException;
+use AppBundle\Service\File\Checker\Exception\RiskyFileException;
+use AppBundle\Service\File\Types\Doc;
 use AppBundle\Service\File\Types\Jpg;
 use AppBundle\Service\File\Types\Pdf;
 use AppBundle\Service\File\Types\Png;
+use AppBundle\Service\File\Types\Tif;
 use AppBundle\Service\File\Types\UploadableFile;
 use AppBundle\Service\File\Types\UploadableFileInterface;
 use Psr\Log\LoggerInterface;
@@ -29,6 +33,16 @@ class FileCheckerFactory
     protected $jpg;
 
     /**
+     * @var Tif
+     */
+    protected $tif;
+
+    /**
+     * @var Doc
+     */
+    protected $doc;
+
+    /**
      * @var LoggerInterface
      */
     protected $logger;
@@ -38,17 +52,23 @@ class FileCheckerFactory
      * @param Pdf $pdf
      * @param Png $png
      * @param Jpg $jpg
-     * @parma Other $other
+     * @param Tif $tif
+     * @param Doc $doc
+     * @param LoggerInterface $logger
      */
     public function __construct(
         Pdf $pdf,
         Png $png,
         Jpg $jpg,
+        Tif $tif,
+        Doc $doc,
         LoggerInterface $logger
     ) {
         $this->pdf = $pdf;
         $this->png = $png;
         $this->jpg = $jpg;
+        $this->tif = $tif;
+        $this->doc = $doc;
         $this->logger = $logger;
     }
 
@@ -59,19 +79,23 @@ class FileCheckerFactory
      */
     public function factory(UploadedFile $uploadedFile)
     {
+        if ($uploadedFile->isExecutable() ||
+            preg_match('/(\.exe)(\.bin)(\.bat)(\.jss)(\.zip)(\.php)/i', $uploadedFile->getClientOriginalName())) {
+            throw new InvalidFileTypeException(s);
+        }
         switch ($uploadedFile->getMimeType()) {
             case 'application/pdf':
                 return $this->pdf->setUploadedFile($uploadedFile);
+            case 'application/msword':
+                return $this->doc->setUploadedFile($uploadedFile);
             case 'image/png':
                 return  $this->png->setUploadedFile($uploadedFile);
             case 'image/jpeg':
                 return $this->jpg->setUploadedFile($uploadedFile);
+            case 'image/tiff':
+                return $this->tif->setUploadedFile($uploadedFile);
             default:
-                $uploadableFile = new UploadableFile($this->logger);
-                $uploadableFile->setUploadedFile($uploadedFile);
-                $uploadableFile->setFileCheckers([]);
-                return $uploadableFile;
-
+                throw new InvalidFileTypeException();
         }
     }
 }
