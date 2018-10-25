e917b7ea09f54837288f8b28a94ab8fd01ac59db
Shaun Lizzio <shaun@lizzio.co.uk>
2018-10-25 14:21:31 +0100

    DCOP-120 removed real guessable passwords to some string to force users to change password

--- app/config/doctrine/DoctrineMigrations/Version20181025074445.php
+++ app/config/doctrine/DoctrineMigrations/Version20181025074445.php
@@ -13,13 +13,13 @@ final class Version20181025074445 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('INSERT INTO dc_user VALUES(1,\'elvis.ciotti@digital.justice.gov.uk\',\'$2y$12$M6AXd9I0x/hdr2rWs31HDOo1frfSJmd9Wk2WKZ9eHVSvmVgLQ8O3K\')');
-        $this->addSql('INSERT INTO dc_user VALUES(2,\'sean.privett@digital.justice.gov.uk\',\'$2y$12$M6AXd9I0x/hdr2rWs31HDOo1frfSJmd9Wk2WKZ9eHVSvmVgLQ8O3K\')');
-        $this->addSql('INSERT INTO dc_user VALUES(3,\'shaun.lizzio@digital.justice.gov.uk\',\'$2y$12$M6AXd9I0x/hdr2rWs31HDOo1frfSJmd9Wk2WKZ9eHVSvmVgLQ8O3K\')');
-        $this->addSql('INSERT INTO dc_user VALUES(4,\'robert.ford@digital.justice.gov.uk\',\'$2y$12$M6AXd9I0x/hdr2rWs31HDOo1frfSJmd9Wk2WKZ9eHVSvmVgLQ8O3K\')');
-        $this->addSql('INSERT INTO dc_user VALUES(5,\'phil.wilson@digital.justice.gov.uk\',\'$2y$12$M6AXd9I0x/hdr2rWs31HDOo1frfSJmd9Wk2WKZ9eHVSvmVgLQ8O3K\')');
-        $this->addSql('INSERT INTO dc_user VALUES(6,\'elizabeth.feenan@digital.justice.gov.uk\',\'$2y$12$M6AXd9I0x/hdr2rWs31HDOo1frfSJmd9Wk2WKZ9eHVSvmVgLQ8O3K\')');
-        $this->addSql('INSERT INTO dc_user VALUES(7,\'stephen.petch@digital.justice.gov.uk\',\'$2y$12$M6AXd9I0x/hdr2rWs31HDOo1frfSJmd9Wk2WKZ9eHVSvmVgLQ8O3K\')');
+        $this->addSql('INSERT INTO dc_user VALUES(1,\'elvis.ciotti@digital.justice.gov.uk\',\'set-me-up\')');
+        $this->addSql('INSERT INTO dc_user VALUES(2,\'sean.privett@digital.justice.gov.uk\',\'set-me-up\')');
+        $this->addSql('INSERT INTO dc_user VALUES(3,\'shaun.lizzio@digital.justice.gov.uk\',\'set-me-up\')');
+        $this->addSql('INSERT INTO dc_user VALUES(4,\'robert.ford@digital.justice.gov.uk\',\'set-me-up\')');
+        $this->addSql('INSERT INTO dc_user VALUES(5,\'phil.wilson@digital.justice.gov.uk\',\'set-me-up\')');
+        $this->addSql('INSERT INTO dc_user VALUES(6,\'elizabeth.feenan@digital.justice.gov.uk\',\'set-me-up\')');
+        $this->addSql('INSERT INTO dc_user VALUES(7,\'stephen.petch@digital.justice.gov.uk\',\'set-me-up\')');
         $this->addSql('ALTER SEQUENCE dc_user_id_seq RESTART WITH 20');
 
         
