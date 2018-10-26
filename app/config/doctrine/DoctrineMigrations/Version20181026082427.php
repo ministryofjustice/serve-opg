11225fe0374325a8e5f758726eb8e6d915b44894
Shaun Lizzio <shaun@lizzio.co.uk>
2018-10-26 10:06:32 +0100

    DCOP-120 removed default date for madeAt

--- app/config/doctrine/DoctrineMigrations/Version20181026082427.php
+++ app/config/doctrine/DoctrineMigrations/Version20181026082427.php
@@ -15,7 +15,6 @@ final class Version20181026082427 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE dc_order ALTER made_at SET DEFAULT \'01-01-2017\'');
         $this->addSql('ALTER TABLE dc_user ADD activation_token VARCHAR(40) DEFAULT NULL');
         $this->addSql('ALTER TABLE dc_user ADD activation_token_created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
     }
@@ -28,6 +27,5 @@ final class Version20181026082427 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE dc_user DROP activation_token');
         $this->addSql('ALTER TABLE dc_user DROP activation_token_created_at');
-        $this->addSql('ALTER TABLE dc_order ALTER made_at SET DEFAULT \'2017-01-01 00:00:00\'');
     }
 }
