ef447530422f9bc0ad5911362e626190cca74d25
Shaun Lizzio <shaun@lizzio.co.uk>
2018-11-19 12:35:17 +0000

    DCOP-154 COP4 only required if non-PA

--- src/AppBundle/Entity/OrderHw.php
+++ src/AppBundle/Entity/OrderHw.php
@@ -14,11 +14,19 @@ class OrderHw extends Order
 
     public function getAcceptedDocumentTypes()
     {
-        return [
+
+        $requiredDocs = [
             Document::TYPE_COURT_ORDER => true,
             Document::TYPE_COP3 => true,
-            Document::TYPE_COP4 => true,
         ];
+
+        // remove COP4 if a PA deputy exists for this order
+        if (!$this->hasDeputyByType(Deputy::DEPUTY_TYPE_PA))
+        {
+            $requiredDocs[Document::TYPE_COP4] = true;
+        }
+
+        return $requiredDocs;
     }
 
     protected function isOrderValid()
