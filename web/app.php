f58bb2de63f8b0c66149c628a4c223d578dbbc9a
Shaun Lizzio <shaun@lizzio.co.uk>
2018-11-20 10:32:11 +0000

    DCOP-157 Added headers

--- web/app.php
+++ web/app.php
@@ -23,5 +23,12 @@ Request::setTrustedProxies(
 );
 
 $response = $kernel->handle($request);
+
+// add additional headers DCOP-157
+//$response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+//$response->headers->set('X-XSS-Protection', '1; mode=block');
+//$response->headers->set('X-Content-Type-Options', 'nosniff');
+//$response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+
 $response->send();
 $kernel->terminate($request, $response);
