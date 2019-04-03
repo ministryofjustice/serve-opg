eb6e614c9769645a4a512056f393431d199c2590
Tom Withers <tomtucka@gmail.com>
2019-04-03 11:24:16 +0100

    Served Orders Report (#197)
    * Basic functionality is now working
    * Refactored tests
    * Updated date to ISO8601

---
+++ src/AppBundle/Service/ReportService.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Order;
+use AppBundle\Repository\OrderRepository;
+use Doctrine\ORM\EntityManager;
+
+class ReportService
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @var OrderRepository
+     */
+    private $orderRepo;
+
+
+    /**
+     * ReportService constructor
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+        $this->orderRepo = $em->getRepository(Order::class);
+    }
+
+    public function generateCsv()
+    {
+        $orders = $this->getOrders();
+
+        $headers = ['DateServed', 'CaseNumber', 'Type'];
+        $ordersCsv = [];
+
+        foreach ($orders as $order) {
+            $ordersCsv[] = ["Date Served" => $order->getServedAt()->format('Y-m-d'),
+                "Case Number" => $order->getClient()->getCaseNumber(),
+                "Type" => $order->getAppointmentType()];
+        }
+
+        $file = fopen("/tmp/orders.csv","w");
+
+        fputcsv($file, $headers);
+
+        foreach($ordersCsv as $line) {
+            fputcsv($file, $line);
+        }
+
+        fclose($file);
+
+        return $file;
+    }
+
+    /**
+     *  Get orders that have been served into Sirius
+     *
+     * @return \AppBundle\Entity\Order[]
+     */
+    public function getOrders()
+    {
+        $filters = [
+            'type' => 'served'
+        ];
+        return $this->orderRepo->getOrders($filters, 1000);
+    }
+}
