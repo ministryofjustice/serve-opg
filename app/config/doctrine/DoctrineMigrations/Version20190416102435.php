5b0427c2c502715f8196c46e15edd5e792097f67
Tom <tomtucka@gmail.com>
2019-04-16 14:26:30 +0100

    213: Add migration to remove cases

---
+++ app/config/doctrine/DoctrineMigrations/Version20190416102435.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190416102435 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("DELETE FROM dc_order o where o.client_id IN 
+                            (select id from client c where c.case_number IN 
+                            ('12637377', '12804927', '12801676', '12588655', '12793003', '12827742', '12827477', '12833148', '12831770', '12842762', '12878811'))");
+        $this->addSql("DELETE FROM client c where c.case_number IN ('12637377', '12804927', '12801676', '12588655', '12793003', '12827742', '12827477', '12833148', '12831770', '12842762', '12878811')");
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
