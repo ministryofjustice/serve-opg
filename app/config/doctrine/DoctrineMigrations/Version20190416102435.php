9f5d7f558f872e3e9ae2988772529bec4c1a187d
Luke Bradbury <luke@nqyr.io>
2019-04-17 09:28:46 +0100

    Update app/config/doctrine/DoctrineMigrations/Version20190416102435.php
    Co-Authored-By: TomTucka <tomtucka@gmail.com>

--- app/config/doctrine/DoctrineMigrations/Version20190416102435.php
+++ app/config/doctrine/DoctrineMigrations/Version20190416102435.php
@@ -13,7 +13,7 @@ final class Version20190416102435 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("DELETE FROM client c where c.case_number IN ('12637377', '12804927', '12801676', '12588655', '12793003', '12827742', '12827477', '12833148', '12831770', '12842762', '12878811')");
+        $this->addSql("DELETE FROM client WHERE case_number IN ('12637377', '12804927', '12801676', '12588655', '12793003', '12827742', '12827477', '12833148', '12831770', '12842762', '12878811')");
 
     }
 
