f35e20bb27a1b71a02a44772793dca139a94488e
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-04-25 11:51:18 +0100

    extracts custom request handling from app.php into index.php

--- new-app/public/index.php
+++ new-app/public/index.php
@@ -6,6 +6,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
+$isLocal = file_exists(__DIR__ . '/../.enableDevMode');
+
+if ($isLocal) {
+    ini_set('display_errors', 'on');
+    Debug::enable();
+    $kernel = new AppKernel('dev', true);
+} else {
+    $kernel = new AppKernel('prod', false);
+}
+
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
@@ -23,5 +33,12 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+
+// add additional headers DCOP-157
+$response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+$response->headers->set('X-XSS-Protection', '1; mode=block');
+$response->headers->set('X-Content-Type-Options', 'nosniff');
+$response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+
 $response->send();
 $kernel->terminate($request, $response);
