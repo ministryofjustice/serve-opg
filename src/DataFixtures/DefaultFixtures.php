f0495e7e5ef3e58f1523407884bc56dfb953bdc4
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-04-26 17:28:01 +0100

    temp fix for behat fixtures

--- src/DataFixtures/DefaultFixtures.php
+++ src/DataFixtures/DefaultFixtures.php
@@ -30,9 +30,30 @@ class DefaultFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+//        @todo move into fixture file rather than hardcode here
+        $users = <<<USERS
+email=elvis.ciotti@digital.justice.gov.uk&password=Abcd1234
+email=sean.privett@digital.justice.gov.uk&password=Abcd1234
+email=shaun.lizzio@digital.justice.gov.uk&password=Abcd1234
+email=robert.ford@digital.justice.gov.uk&password=Abcd1234
+USERS;
+
+//        @todo move into fixture file rather than hardcode here
+        $cases = <<<CASES
+number=4865226T&name=Peter Bloggs&type=both
+number=88744573&name=Victoria Brady&type=PF
+number=1267847T&name=Thomas Jefferson&type=HW
+number=9258173T&name=Susan Grindle&type=both
+number=03427488&name=Lia Shelton&type=PF
+number=51934429&name=Abdullah Lang&type=PF
+number=6554033T&name=Angela Walker&type=PF
+number=98848454&name=Justine Henderson&type=PF
+number=14564190&name=Marcus Cruz&type=PF
+CASES;
+
         // users
         $repo = $manager->getRepository(User::class);
-        $userLines = array_filter(explode("\n", getenv('DC_FIXURES_USERS')));
+        $userLines = array_filter(explode("\n", $users));
         foreach ($userLines as $userLine) {
             parse_str($userLine, $user);
             if (!empty($user['email']) && !$repo->findOneBy(['email' => $user['email']])) {
@@ -46,7 +67,7 @@ class DefaultFixtures extends Fixture
 
         // clients
         $repo = $manager->getRepository(Client::class);
-        $caseLines = array_filter(explode("\n", getenv('DC_FIXURES_CASES')));
+        $caseLines = array_filter(explode("\n", $cases));
         foreach ($caseLines as $caseLine) {
             parse_str($caseLine, $case);
             if (!$client = $repo->findOneBy(['caseNumber' => $case['number']])) {
