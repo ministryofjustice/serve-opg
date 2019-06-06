b7d4fef302e4a046a2c18a5b4c4bce8a9d339e25
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-06-06 13:53:53 +0100

    fix test file location

--- tests/Controller/OrderControllerTest.php
+++ tests/Controller/OrderControllerTest.php
@@ -7,11 +7,11 @@ use App\Service\DocumentService;
 use App\Service\OrderService;
 use Doctrine\ORM\EntityManager;
 use Prophecy\Prophecy\ObjectProphecy;
-use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
-class OrderControllerTest extends TestCase
+class OrderControllerTest extends WebTestCase
 {
     public function setUp()
     {
@@ -29,7 +29,11 @@ class OrderControllerTest extends TestCase
             $this->documentService->reveal()
         );
 
-        $fileLocation = '../TestData/validCO.docx';
+        self::bootKernel();
+        $container = self::$container;
+        $projectDir = $container->get('kernel')->getProjectDir();
+        $fileLocation = $projectDir . '/tests/TestData/validCO.docx';
+
         $file = new UploadedFile($fileLocation, 'validCO.docx', 'application/msword', null);
         $expectedJSONResponse = json_encode(['valid' => true]);
 
