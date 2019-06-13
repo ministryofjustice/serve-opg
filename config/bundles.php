ffe370da49091177b4c95a174c5617227c713c61
Alex Saunders <47983793+acsauk-mojd@users.noreply.github.com>
2019-06-13 12:45:26 +0100

    Revert "implement webpack (#288)"
    This reverts commit 4ef588f8ecc2537535f1cc0ea1e44417c91031c8.

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
