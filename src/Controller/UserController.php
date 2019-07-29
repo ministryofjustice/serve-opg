0a9a6585a596a1605966205cc273a965c15a3508
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-07-29 11:03:26 +0100

    drops unused dependency

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -35,10 +34,6 @@ class UserController extends AbstractController
      * @var UserPasswordEncoderInterface
      */
     private $encoder;
-    /**
-     * @var UrlGeneratorInterface
-     */
-    private $urlGenerator;
 
     /**
      * UserController constructor.
