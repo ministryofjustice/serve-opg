9855c06c139f3da97157b7285e8e465ddd72ee8f
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-07-31 14:28:53 +0100

    drops headers argument from SiriusClient as not in use

--- src/Service/SiriusClient.php
+++ src/Service/SiriusClient.php
@@ -13,10 +13,8 @@ class SiriusClient extends GuzzleHttpClient
      * SiriusClient constructor.
      * @param $args array of arguments
      */
-    public function __construct($args, $headers)
+    public function __construct($args)
     {
-        // see https://github.com/symfony/yaml/commit/9744e5991d1436620c3d01b507160b31803546f3#diff-037865b8555f7db5ac9338cfb5be7466
-        $args['headers'] = $headers;
         parent::__construct($args);
     }
 }
