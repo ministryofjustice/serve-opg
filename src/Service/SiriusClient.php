44d8a3a79df3ed489491921139596288e9b2ee5c
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-07-31 09:42:23 +0100

    fixes multiline yaml block deprecation, standardises namespaces for controller tests

--- src/Service/SiriusClient.php
+++ src/Service/SiriusClient.php
@@ -13,8 +13,10 @@ class SiriusClient extends GuzzleHttpClient
      * SiriusClient constructor.
      * @param $args array of arguments
      */
-    public function __construct($args)
+    public function __construct($args, $headers)
     {
+        // see https://github.com/symfony/yaml/commit/9744e5991d1436620c3d01b507160b31803546f3#diff-037865b8555f7db5ac9338cfb5be7466
+        $args['headers'] = $headers;
         parent::__construct($args);
     }
 }
