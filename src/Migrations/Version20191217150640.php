7422ebb2dc6260be6de70ffad3a70d63881da3bd
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-12-17 15:47:24 +0000

    Adds new User to Serve

---
+++ src/Migrations/Version20191217150640.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191217150640 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $now = date("Y-m-d h:m:s");
+
+        $this->addSql("INSERT INTO dc_user (id, email, password, created_at, roles) VALUES(nextval('dc_user_id_seq'), 'Al-Amin.Vintura@justice.gov.uk', 'set-me-up', '${now}', 'a:0:{}')");
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql("DELETE FROM dc_user WHERE email = 'Al-Amin.Vintura@justice.gov.uk';");
+    }
+}
