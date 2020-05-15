c7c66cea6c34016ac3954a12a9c9a878a3ebcb86
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-05-15 17:52:46 +0100

    Fix tests

--- tests/Service/SiriusServiceTest.php
+++ tests/Service/SiriusServiceTest.php
@@ -61,7 +61,7 @@ class SiriusServiceTest extends MockeryTestCase
 
     public function testPingFailure()
     {
-        $this->mockHttpClient->get('/health-check/service-status', Argument::cetera())->shouldBeCalled()->willThrow(new \RuntimeException());
+        $this->mockHttpClient->get('health-check/service-status', Argument::cetera())->shouldBeCalled()->willThrow(new \RuntimeException());
 
         $this->sut = new SiriusService(
             $this->mockEntityManager->reveal(),
