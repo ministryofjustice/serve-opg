fec81f4fffee5988ddf98772cc4a55ec21b03823
james.warren <james.warren@digital.justice.gov.uk>
2020-11-24 17:31:24 +0000

    ddpb-3773 make variables settable

--- src/Behat/FeatureContext.php
+++ src/Behat/FeatureContext.php
@@ -25,17 +25,10 @@ class FeatureContext extends MinkContext implements Context
      */
     private string $behatPassword;
 
-//    /**
-//     * FeatureContext constructor.
-//     * @param string $behatPassword
-//     * @param string $behatPasswordNew
-//     */
     public function __construct()
     {
-//        $this->behatPassword = $behatPassword;
-//        $this->behatPasswordNew = $behatPasswordNew;
         $this->behatPassword = getenv("BEHAT_PASSWORD");
-        $this->behatPasswordNew = getenv("BEHAT_PASSWORD_NEW");
+        $this->behatPasswordNew = $this->behatPassword . "9";
     }
 
     /**
