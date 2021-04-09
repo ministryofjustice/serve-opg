85d4d2745a57032fcea5ce6999e38bdf9285e05e
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-04-09 14:44:43 +0100

    created server

--- serve-api/go.mod
+++ serve-api/go.mod
@@ -1,3 +1,5 @@
 module serve-api
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
--- serve-api/main.go
+++ serve-api/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Hello world")
+	sm := mux.NewRouter().PathPrefix("serve-api").Subrouter()
+	l := log.New(os.Stdout, "serve-api ", log.LstdFlags)
+	s := &http.Server{
+		Addr:         ":8000",
+		Handler:      sm,
+		ErrorLog:     l,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 15 * time.Minute,
+	}
+
+	// start the server
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	l.Println("Recieve terminate, gracefully shutting down", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
