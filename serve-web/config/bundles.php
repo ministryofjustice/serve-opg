a67a34698a7038efe3d5db309d244fb2359e8efa
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-08-02 14:31:00 +0100

    Use symfony guard and updated symfony flex

--- serve-web/config/bundles.php
+++ serve-web/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
