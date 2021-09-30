85e20ee20cb420e1cfddbef6bc427461de99083b
Alex Saunders <acsauk@gmail.com>
2021-09-30 17:30:00 +0100

    Break stats panel out to partials and macros

--- serve-web/src/Controller/CaseController.php
+++ serve-web/src/Controller/CaseController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Order;
 use App\Repository\OrderRepository;
 use App\Service\Security\LoginAttempts\Checker;
+use DateTime;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -53,6 +54,47 @@ class CaseController extends AbstractController
             'counts' => [
                 'pending' => $this->orderRepo->getOrdersCount(['type' => 'pending'] + $filters),
                 'served' => $this->orderRepo->getOrdersCount(['type' => 'served'] + $filters),
+            ],
+            'toDoStats' => [
+                'totalOrders' => [
+                    'amount' => '6526',
+                    'description' => 'Total court order backlog'
+                ],
+                'select' => [
+                    'label' => 'Show backlog by',
+                    'options' => [
+                        ['value' => 'year_breakdown', 'description' => 'Year Breakdown'],
+                        ['value' => 'order_type', 'description' => 'Order Type'],
+                        ['value' => 'order_status', 'description' => 'Order Status'],
+                    ]
+                ],
+                'breakdownItems' => [
+                    ['numberOfOrders' => '3456', 'dateFrom' => new DateTime('1 January 2018'), 'dateTo' => new DateTime('31 December 2018')],
+                    ['numberOfOrders' => '1447', 'dateFrom' => new DateTime('1 January 2019'), 'dateTo' => new DateTime('31 December 2019')],
+                    ['numberOfOrders' => '2497', 'dateFrom' => new DateTime('1 January 2020'), 'dateTo' => new DateTime('31 December 2020')],
+                    ['numberOfOrders' => '3456', 'dateFrom' => new DateTime('1 January 2021'), 'dateTo' => new DateTime('now')],
+                ]
+            ],
+            'servedStats' => [
+                'totalOrders' => [
+                    'amount' => '27568',
+                    'description' => 'Total orders served'
+                ],
+                'select' => [
+                    'label' => 'Show served Court Orders by',
+                    'options' => [
+                        ['value' => 'year_breakdown', 'description' => 'Year Breakdown'],
+                        ['value' => 'order_type', 'description' => 'Order Type'],
+                        ['value' => 'order_status', 'description' => 'Order Status'],
+                    ]
+                ],
+                'breakdownItems' => [
+                    ['numberOfOrders' => '6789', 'dateFrom' => new DateTime('1 January 2018'), 'dateTo' => new DateTime('31 December 2018')],
+                    ['numberOfOrders' => '9221', 'dateFrom' => new DateTime('1 January 2019'), 'dateTo' => new DateTime('31 December 2019')],
+                    ['numberOfOrders' => '8132', 'dateFrom' => new DateTime('1 January 2020'), 'dateTo' => new DateTime('31 December 2020')],
+                    ['numberOfOrders' => '3456', 'dateFrom' => new DateTime('1 January 2021'), 'dateTo' => new DateTime('now')],
+                    ['numberOfOrders' => '101'],
+                ]
             ]
         ]);
     }
