08068083e2d1af3aac3010bd25c32fe441380868
Alex Saunders <acsauk@gmail.com>
2021-10-01 16:22:25 +0100

    Test fixes

--- serve-web/tests/Service/Stats/AssemblerTest.php
+++ serve-web/tests/Service/Stats/AssemblerTest.php
@@ -17,7 +17,7 @@ class AssemblerTest extends TestCase
     /**
      * @test
      */
-    public function assembleOrderMadePeriodStats()
+    public function assembleOrderMadePeriodStats_pending()
     {
         $now = new DateTime('7 October 2020');
 
@@ -29,14 +29,16 @@ class AssemblerTest extends TestCase
         $to2 = new DateTime('31 December 2019');
         $to3 = $now;
 
+        $orderStatus = Stats::STAT_STATUS_TO_DO;
+
         /** @var ObjectProphecy|OrderRepository $repo */
         $repo = self::prophesize(OrderRepository::class);
-        $repo->getOrdersCountByMadeDatePeriods($from1, $to1)->shouldBeCalled()->willReturn(25);
-        $repo->getOrdersCountByMadeDatePeriods($from2, $to2)->shouldBeCalled()->willReturn(75);
-        $repo->getOrdersCountByMadeDatePeriods($from3, $to3)->shouldBeCalled()->willReturn(100);
+        $repo->getOrdersCountByMadeDatePeriods($from1, $to1, $orderStatus)->shouldBeCalled()->willReturn(25);
+        $repo->getOrdersCountByMadeDatePeriods($from2, $to2, $orderStatus)->shouldBeCalled()->willReturn(75);
+        $repo->getOrdersCountByMadeDatePeriods($from3, $to3, $orderStatus)->shouldBeCalled()->willReturn(100);
 
         $sut = new Assembler($repo->reveal());
-        $actualResult = $sut->assembleOrderMadePeriodStats($now);
+        $actualResult = $sut->assembleOrderMadePeriodStats($orderStatus, $now);
 
         $expectedResult = (new Stats())
             ->addOrderMadePeriodStat(
@@ -56,6 +58,53 @@ class AssemblerTest extends TestCase
                     ->setFrom($from3)
                     ->setTo($to3)
                     ->setNumberOfOrders(100)
+            );
+
+        self::assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @test
+     */
+    public function assembleOrderMadePeriodStats_served()
+    {
+        $now = new DateTime('7 October 2019');
+
+        $from1 = new DateTime('1 January 2018');
+        $from2 = new DateTime('1 January 2019');
+
+        $to1 = new DateTime('31 December 2018');
+        $to2 = $now;
+
+        $orderStatus = Stats::STAT_STATUS_SERVED;
+
+        /** @var ObjectProphecy|OrderRepository $repo */
+        $repo = self::prophesize(OrderRepository::class);
+        $repo->getOrdersCountByMadeDatePeriods($from1, $to1, $orderStatus)->shouldBeCalled()->willReturn(25);
+        $repo->getOrdersCountByMadeDatePeriods($from2, $to2, $orderStatus)->shouldBeCalled()->willReturn(75);
+        $repo->getOrdersCountByMadeDatePeriods($now, $now, $orderStatus)->shouldBeCalled()->willReturn(5);
+
+        $sut = new Assembler($repo->reveal());
+        $actualResult = $sut->assembleOrderMadePeriodStats($orderStatus, $now);
+
+        $expectedResult = (new Stats($orderStatus))
+            ->addOrderMadePeriodStat(
+                (new OrderMadePeriodStat())
+                    ->setFrom($from1)
+                    ->setTo($to1)
+                    ->setNumberOfOrders(25)
+            )
+            ->addOrderMadePeriodStat(
+                (new OrderMadePeriodStat())
+                    ->setFrom($from2)
+                    ->setTo($to2)
+                    ->setNumberOfOrders(75)
+            )
+            ->addOrderMadePeriodStat(
+                (new OrderMadePeriodStat())
+                    ->setFrom($now)
+                    ->setTo($now)
+                    ->setNumberOfOrders(5)
             );
 
         self::assertEquals($expectedResult, $actualResult);
