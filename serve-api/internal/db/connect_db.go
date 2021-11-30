12d28f2d1b84e66314fec4b54bcb9ca8fb984c63
Gugandeep Chani <Gugandeep.Chani@digital.justice.gov.uk>
2021-11-30 16:16:12 +0000

    Added Client Model Gorm Migration with initial test

---
+++ serve-api/internal/db/connect_db.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() *gorm.DB {
+	db_user := os.Getenv("POSTGRES_API_DB_USER")
+	db_pswd := os.Getenv("POSTGRES_PASSWORD")
+	db_name := os.Getenv("POSTGRES_DB")
+
+	dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=5432 sslmode=disable", db_user, db_pswd, db_name)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	return db
+}
+
+func Test() []string {
+
+	db := Connect()
+
+	rows, err := db.Raw("SELECT * FROM dc_user").Rows()
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	columns, err := rows.Columns()
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	return columns
+}
