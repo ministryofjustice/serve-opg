03a070b9dc37b41b894be12f81864994a183d92b
Gugandeep Chani <gugandeep.chani@digital.justice.gov.uk>
2022-05-17 14:53:51 +0100

    Added additional test case

--- serve-api/internal/csv/parse.go
+++ serve-api/internal/csv/parse.go
@@ -1,6 +1,9 @@
 package csv
 
 import (
+	"encoding/csv"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -14,6 +17,53 @@ type Case struct {
 	OrderNo    int
 }
 
-func Parse(filePath string) (Case, error) {
-	return Case{}, nil
+func Parse(filePath string) (*Case, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+
+	record, err := csvReader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	record, err = csvReader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	importedCase := Case{
+		CaseNumber: record[0],
+		Forename:   record[1],
+		Surname:    record[2],
+		OrderType:  parseStringAsInt(record[3]),
+		MadeDate:   parseStringAsDate(record[4]),
+		IssueDate:  parseStringAsDate(record[5]),
+		OrderNo:    parseStringAsInt(record[6]),
+	}
+
+	return &importedCase, nil
+}
+
+func parseStringAsInt(str string) int {
+	integer, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return integer
+}
+
+func parseStringAsDate(str string) time.Time {
+	stringLayout := "01-Jan-2022"
+	date, err := time.Parse(stringLayout, str)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return date
 }
--- serve-api/internal/csv/parse_test.go
+++ serve-api/internal/csv/parse_test.go
@@ -2,6 +2,7 @@ package csv
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -17,6 +18,20 @@ func TestParse(t *testing.T) {
 			desc:          "1. Passing in a valid csv filepath",
 			filePath:      "cases.csv",
 			expectedCase:  Case{},
+			expectedError: nil,
+		},
+		{
+			desc:     "2. Passing in a CSV that contains a single row",
+			filePath: "cases.csv",
+			expectedCase: Case{
+				CaseNumber: "12345678",
+				Forename:   "John",
+				Surname:    "Smith",
+				OrderType:  1,
+				MadeDate:   time.Date(2022, 5, 17, 0, 0, 0, 0, time.UTC),
+				IssueDate:  time.Date(2022, 5, 17, 0, 0, 0, 0, time.UTC),
+				OrderNo:    1000,
+			},
 			expectedError: nil,
 		},
 	}
