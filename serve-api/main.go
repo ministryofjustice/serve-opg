f31986c54c5b76a09de65611f2a74da27aaf6327
Stephen Hinett <me@stephenhinett.co.uk>
2022-07-08 15:08:35 +0100

    [SER-166] refactor docker files and fixed GOARCH environment variable

--- serve-api/main.go
+++ serve-api/main.go
@@ -24,6 +24,10 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	sm.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello you!"))
+	})
+
 	// setting up the http server
 	s := &http.Server{
 		Addr:         ":9000",
