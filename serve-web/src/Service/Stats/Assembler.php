72d209935c2c36b453ef4ded7c6245aca0567387
Alex Saunders <acsauk@gmail.com>
2022-07-22 12:04:12 +0100

    Tweak layout of stats panel and drop filter

--- serve-web/src/Service/Stats/Assembler.php
+++ serve-web/src/Service/Stats/Assembler.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Service\Stats;
 
-
 use App\Repository\OrderRepository;
 use App\Service\Stats\Model\OrderMadePeriodStat;
 use App\Service\Stats\Model\Stats;
