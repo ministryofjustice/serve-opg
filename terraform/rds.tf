02003cf7102425b7cbe5559ab1a44888b1afd3db
Tom Gulliver <tgulliver97@gmail.com>
2022-11-24 12:46:27 +0000

    SER-181 - retry version

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -26,7 +26,6 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   cluster_identifier           = aws_rds_cluster.serve_opg.id
   instance_class               = terraform.workspace == "production" ? "db.r4.large" : "db.r5.large"
   engine                       = aws_rds_cluster.serve_opg.engine
-  engine_version               = aws_rds_cluster.serve_opg.engine_version
   performance_insights_enabled = true
   monitoring_role_arn          = aws_iam_role.enhanced_monitoring.arn
   monitoring_interval          = 60
