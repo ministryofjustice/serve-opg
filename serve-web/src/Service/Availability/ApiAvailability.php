6221eb05c69e4bb82652a925dffeccbf77508c67
Tom Gulliver <tgulliver97@gmail.com>
2023-07-25 19:51:01 +0100

    SER-206 - Refactor api healthchecks

--- serve-web/src/Service/Availability/ApiAvailability.php
+++ serve-web/src/Service/Availability/ApiAvailability.php
@@ -17,7 +17,7 @@ class ApiAvailability extends ServiceAvailabilityAbstract
     {
         try {
             $data = $this->restClient->get('health-check/service', 'array');
-            // API not healtyh
+            // API not healthy
             if (JSON_ERROR_NONE !== json_last_error() || !isset($data['healthy'])) {
                 $this->isHealthy = false;
                 $this->errors = 'Cannot read API status. '.json_last_error_msg();
