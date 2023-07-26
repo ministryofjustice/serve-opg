0cfe484b8fc5ec3002f63b9c4659fbbd1bf60d25
Tom Gulliver <tgulliver97@gmail.com>
2023-07-26 11:05:37 +0100

    SER-206 - standardise alarm names

---
+++ serve-web/tests/Controller/HealthControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Phpunit\Helpers\AbstractControllerTestCase;
+
+class HealthControllerTest extends AbstractControllerTestCase
+{
+
+    public function testServiceHealth()
+    {
+        $this->client->request('GET', '/health-check/service');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+
+    public function testContainerHealth()
+    {
+        $this->client->request('GET', '/health-check');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+}
