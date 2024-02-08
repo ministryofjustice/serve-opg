0237e276dd421e616ee911575ecb056afc398673
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-02-08 12:00:09 +0000

    ddls-134b add the sg rules for db access

--- terraform/cloud9.tf
+++ terraform/cloud9.tf
@@ -1,6 +1,6 @@
 locals {
-  cloud9_users_from_secret = jsondecode(data.aws_secretsmanager_secret_version.cloud9_users.secret_string)["user_names"]
-  cloud9_owner_from_secret = jsondecode(data.aws_secretsmanager_secret_version.cloud9_users.secret_string)["owner"]
+  cloud9_users_from_secret         = jsondecode(data.aws_secretsmanager_secret_version.cloud9_users.secret_string)["user_names"]
+  cloud9_owner_from_secret         = jsondecode(data.aws_secretsmanager_secret_version.cloud9_users.secret_string)["owner"]
   cloud9_owner_session_from_secret = jsondecode(data.aws_secretsmanager_secret_version.cloud9_users.secret_string)["owner_session"]
   cloud9_users_for_each = {
     for user, user in local.cloud9_users_from_secret : user => user
@@ -25,4 +25,12 @@ resource "aws_cloud9_environment_membership" "shared" {
   environment_id = aws_cloud9_environment_ec2.shared.id
   permissions    = "read-write"
   user_arn       = "arn:aws:iam::${local.account_id}:assumed-role/operator/${each.value}"
+}
+
+data "aws_security_group" "cloud9" {
+  depends_on = [aws_cloud9_environment_ec2.shared]
+  filter {
+    name   = "tag:aws:cloud9:environment"
+    values = [aws_cloud9_environment_ec2.shared.id]
+  }
 }
