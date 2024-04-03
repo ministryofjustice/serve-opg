fe9849760d63190397eedb320ca2cb495c907b2e
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-04-03 10:47:23 +0100

    SER-214 fixes for tflint

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -29,3 +29,35 @@ resource "aws_secretsmanager_secret" "cloud9_users" {
 data "aws_secretsmanager_secret_version" "cloud9_users" {
   secret_id = aws_secretsmanager_secret.cloud9_users.id
 }
+
+data "aws_secretsmanager_secret" "sirius_api_email" {
+  name = "sirius_api_email_${terraform.workspace}"
+}
+
+data "aws_secretsmanager_secret_version" "sirius_api_email" {
+  secret_id = data.aws_secretsmanager_secret.sirius_api_email.id
+}
+
+data "aws_secretsmanager_secret" "public_api_password" {
+  name = data.aws_secretsmanager_secret_version.sirius_api_email.secret_string
+}
+
+data "aws_secretsmanager_secret" "notification_api_key" {
+  name = "notification_api_key"
+}
+
+data "aws_secretsmanager_secret" "os_places_api_key" {
+  name = "os_places_api_key"
+}
+
+data "aws_secretsmanager_secret" "symfony_app_secret" {
+  name = "symfony_app_secret"
+}
+
+data "aws_secretsmanager_secret" "database_password" {
+  name = "database_password"
+}
+
+data "aws_secretsmanager_secret_version" "database_password" {
+  secret_id = data.aws_secretsmanager_secret.database_password.id
+}
