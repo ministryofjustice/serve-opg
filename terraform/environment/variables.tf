60ffed6902ee38d4f54a7e852252f0ae76f03103
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-04-22 17:58:38 +0100

    SER-165 use branch based environments

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -7,7 +7,6 @@ variable "accounts" {
   type = map(
     object({
       account_id          = string
-      prefix              = string
       behat_controller    = number
       sirius_api          = string
       sirius_bucket       = string
@@ -27,9 +26,9 @@ variable "accounts" {
 
 locals {
   environment        = terraform.workspace
-  account            = var.accounts[local.environment]
+  account            = contains(keys(var.accounts), local.environment) ? var.accounts[local.environment] : var.accounts["default"]
   management         = "311462405659"
-  dns_prefix         = local.account.prefix
+  dns_prefix         = local.environment == "production" ? "serve" : "${local.environment}.serve"
   sirius_role        = var.SIRIUS_ROLE == "serve-assume-role-ci" ? "${var.SIRIUS_ROLE}-${local.environment}" : var.SIRIUS_ROLE
   default_allow_list = local.account.ip_whitelist ? module.allow_list.moj_sites : tolist(["0.0.0.0/0"])
 
