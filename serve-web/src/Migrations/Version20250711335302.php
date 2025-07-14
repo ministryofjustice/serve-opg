9d98bf26d51d413c5cc9b6f66ac010760895ac6c
Matty Cooke <matthew.cooke@justice.gov.uk>
2025-07-14 14:10:04 +0100

    Added read permissions

--- serve-web/src/Migrations/Version20250711335302.php
+++ serve-web/src/Migrations/Version20250711335302.php
@@ -32,6 +32,10 @@ BEGIN
      GRANT rds_iam TO "$username";
   END IF;
 
+  IF EXISTS (SELECT * FROM pg_roles WHERE rolname = 'pg_read_all_data') THEN
+     GRANT pg_read_all_data TO "$username";
+  END IF;
+  
   ALTER USER "$username" SET log_statement = 'all';
 
 END
