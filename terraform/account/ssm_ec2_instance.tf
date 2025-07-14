00ef6ec42c67985cea6e55aed719b76828b159db
Matty Cooke <matthew.cooke@justice.gov.uk>
2025-07-14 10:16:19 +0100

    fixing up ssm_ec2_instance file

--- terraform/account/ssm_ec2_instance.tf
+++ terraform/account/ssm_ec2_instance.tf
@@ -1,46 +1,41 @@
 module "ssm_ec2_instance_operator" {
-  source = "./modules/ssm_ec2_instance"
-
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.private[0].id
-  name          = "operator"
-  tags          = local.default_tags
-  //kms_key_id    = module.logs_kms.eu_west_1_target_key_arn
+  source           = "./modules/ssm_ec2_instance"
+  instance_type    = "t3.micro"
+  subnet_id        = aws_subnet.private[0].id
+  name             = "operator"
+  tags             = local.default_tags
   instance_profile = data.aws_iam_instance_profile.operator.name
-  vpc_id = aws_default_vpc.default.id
+  vpc_id           = aws_default_vpc.default.id
 }
 
 data "aws_iam_instance_profile" "operator" {
-   name = "operator"
+  name = "operator"
 }
 
 data "aws_iam_role" "operator" {
   name = "operator"
 }
 
-# SSM Core Statements
-
+# Attach AmazonSSMManagedInstanceCore to role
 resource "aws_iam_role_policy_attachment" "ssm_core_role_policy_document" {
   role       = data.aws_iam_role.operator.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Start EC2 Statements
-
+# Policy Document for EC2 Start/Stop
 data "aws_iam_policy_document" "start_ec2" {
   statement {
-    sid    = "AllowStartStopSpecificInstance"
-    effect = "Allow"
-
+    sid     = "AllowStartStopSpecificInstance"
+    effect  = "Allow"
     actions = [
       "ec2:StartInstances",
-      "ec2:StopInstances"
+      "ec2:StopInstances",
     ]
-
     resources = [module.ssm_ec2_instance_operator.ssm_instance_arn]
   }
 }
 
+# Create and attach custom EC2 control policy
 resource "aws_iam_policy" "start_ec2" {
   name   = "operator-ssm-policy"
   policy = data.aws_iam_policy_document.start_ec2.json
