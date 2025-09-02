ef590c566d27e159bec3234007e5c20403dac14a
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-09-02 13:36:59 +0100

    match up with sirius

--- terraform/environment/event_bus.tf
+++ terraform/environment/event_bus.tf
@@ -16,5 +16,7 @@ resource "aws_cloudwatch_event_target" "to_sirius_bus" {
   rule           = aws_cloudwatch_event_rule.forward_to_sirius.name
   event_bus_name = aws_cloudwatch_event_bus.serve.name
 
-  arn = "arn:aws:events:eu-west-1:${local.account.sirius_account}:event-bus/${local.account.account_name}-supervision"
+  arn = "arn:aws:events:eu-west-1:${local.account.sirius_account}:event-bus/ddls-856-supervision"
 }
+
+#  arn = "arn:aws:events:eu-west-1:${local.account.sirius_account}:event-bus/${local.account.account_name}-supervision"
