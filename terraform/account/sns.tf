162e6bd5a1875376a3f438e5f876df2b218f0ddc
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-09-18 10:26:33 +0100

    global sns queue added

--- terraform/account/sns.tf
+++ terraform/account/sns.tf
@@ -36,6 +36,14 @@ resource "aws_sns_topic_subscription" "serve_slack_notifications_lambda" {
   endpoint  = aws_lambda_function.serve_opg_notify_slack.arn
 }
 
+resource "aws_sns_topic_subscription" "serve_slack_notifications_lambda_global" {
+  provider = aws.us-east-1
+
+  topic_arn = aws_sns_topic.serve_slack_notifications_global.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.serve_opg_notify_slack.arn
+}
+
 resource "aws_lambda_permission" "serve_slack_notifications_allow" {
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
@@ -46,5 +54,50 @@ resource "aws_lambda_permission" "serve_slack_notifications_allow" {
     replace_triggered_by = [
       aws_lambda_function.serve_opg_notify_slack
     ]
+  }
+}
+
+resource "aws_lambda_permission" "serve_slack_global_notifications_allow" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.serve_opg_notify_slack.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.serve_slack_notifications_global.arn
+  lifecycle {
+    replace_triggered_by = [
+      aws_lambda_function.serve_opg_notify_slack
+    ]
+  }
+}
+
+# Slack Alerts Global Topic
+#trivy:ignore:avd-aws-0095 - Can't do cross region SNS encryption
+resource "aws_sns_topic" "serve_slack_notifications_global" {
+  provider     = aws.us-east-1
+  name         = "serve-slack-notifications-global"
+  display_name = "Serve Slack Notifications Global"
+  tags = merge(
+    local.default_tags,
+    { Name = "serve-slack-notifications-global" },
+  )
+}
+
+resource "aws_sns_topic_policy" "serve_slack_notifications_global" {
+  arn    = aws_sns_topic.serve_slack_notifications_global.arn
+  policy = data.aws_iam_policy_document.serve_slack_notifications_global.json
+}
+
+data "aws_iam_policy_document" "serve_slack_notifications_global" {
+  statement {
+    sid    = "Publish SNS"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+
+    actions   = ["sns:Publish"]
+    resources = [aws_sns_topic.serve_slack_notifications_global.arn]
   }
 }
