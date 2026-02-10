1b80b225eb618b54e2198e1574909e986d495222
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-02-10 12:11:36 +0000

    Update docker images and cert config, remove waitforit and use health checks

--- serve-web/docker/app/health-check.sh
+++ serve-web/docker/app/health-check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if cgi-fcgi -bind -connect 127.0.0.1:9000; then
+if REQUEST_URI=/login SCRIPT_NAME=/login SCRIPT_FILENAME=/var/www/public/index.php REMOTE_ADDR=127.0.0.1 REQUEST_METHOD=GET SERVER_NAME=app cgi-fcgi -bind -connect 127.0.0.1:9000; then
 	exit 0
 fi
 
