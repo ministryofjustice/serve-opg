ba37c8d6db5eebc55bb7f3b714eedf3b55c3278b
Matty Cooke <matthew.cooke@justice.gov.uk>
2026-04-13 10:33:26 +0100

    remove db

--- terraform/account/vpc.tf
+++ terraform/account/vpc.tf
@@ -21,12 +21,3 @@ resource "aws_default_vpc_dhcp_options" "default" {
 resource "aws_iam_service_linked_role" "ecs" {
   aws_service_name = "ecs.amazonaws.com"
 }
-
-resource "aws_db_subnet_group" "database" {
-  name       = "data-subnet-group-${local.account.name}"
-  subnet_ids = module.network.data_subnets[*].id
-  tags = merge(
-    local.default_tags,
-    { Name = "data-subnet-group-${local.account.name}" },
-  )
-}
