ce9f601c2051f2ebe10eedb4eaf291d0332e1ab8
Jim <58939809+jamesrwarren@users.noreply.github.com>
2026-04-23 15:19:00 +0100

    DDLS-1342 put development into shared firewall (#871)

--- terraform/account/network.tf
+++ terraform/account/network.tf
@@ -113,13 +113,12 @@ locals {
   firewall_config = lookup(local.account_level_configurations, terraform.workspace, local.account_level_configurations["production"])
   account_level_configurations = {
     development = {
-      network_firewall_enabled      = true
-      none_matching_traffic_action  = "alert"
-      shared_firewall_configuration = null
-      #      shared_firewall_configuration = {
-      #        account_id   = "679638075911"
-      #        account_name = "development"
-      #      }
+      network_firewall_enabled     = true
+      none_matching_traffic_action = "drop"
+      shared_firewall_configuration = {
+        account_id   = "679638075911"
+        account_name = "development"
+      }
     }
     preproduction = {
       network_firewall_enabled      = false
