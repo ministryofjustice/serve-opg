5ff7f53eee70f5b038cbe7dd59179260cef4c822
jamesrwarren <james.warren@digital.justice.gov.uk>
2026-05-20 14:58:18 +0100

    snyk iac scanning

---
+++ terraform/environment/bad.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "bad" {
+  name = "bad-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "ssh_bad" {
+  name = "ssh-open"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
