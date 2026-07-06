631d33f19fd2116961e6848c3a7c11044c9d051b
Jim <58939809+jamesrwarren@users.noreply.github.com>
2026-07-06 15:43:59 +0100

    Upgrade the TLS (#945)

--- terraform/environment/load_balancer.tf
+++ terraform/environment/load_balancer.tf
@@ -44,7 +44,7 @@ resource "aws_lb_listener" "frontend_listen" {
   load_balancer_arn = aws_lb.frontend_lb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate.serve.arn
   depends_on        = [aws_route53_record.serve_validation]
 
