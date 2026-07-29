1ad08f9b08c885dafe5f13ee2ab927a3969486c8
Matty Cooke <matthew.cooke@justice.gov.uk>
2026-07-29 19:21:18 +0100

    fix

--- terraform/environment/ecs_task_orchestration.tf
+++ terraform/environment/ecs_task_orchestration.tf
@@ -2,7 +2,7 @@
 module "restore" {
   source = "./modules/task"
   name   = "restore"
-  user = "orchestration",
+  user = "orchestration"
 
   cluster_name          = aws_ecs_cluster.serve_opg.name
   cpu                   = 2048
@@ -45,8 +45,8 @@ locals {
 module "backup" {
   source = "./modules/task"
   name   = "backup"
-  
-  user = "orchestration",
+
+  user = "orchestration"
   cluster_name          = aws_ecs_cluster.serve_opg.name
   cpu                   = 2048
   memory                = 4096
