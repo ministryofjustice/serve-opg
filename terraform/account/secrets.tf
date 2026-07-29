5b321b7855c29106416063a92daee0f91a196115
jamesrwarren <james.warren@digital.justice.gov.uk>
2026-07-29 14:10:59 +0100

    DDLS-1576b bring in secrets not managed by terraform

--- terraform/account/secrets.tf
+++ terraform/account/secrets.tf
@@ -15,3 +15,79 @@ resource "aws_secretsmanager_secret" "slack_webhooks" {
   description = "Slack webhooks for notifications"
   tags        = local.default_tags
 }
+
+resource "aws_secretsmanager_secret" "public_api_password" {
+  name        = "sirius_public_api_password"
+  description = "Sirius public API password"
+  tags        = local.default_tags
+}
+
+resource "aws_secretsmanager_secret" "notification_api_key" {
+  name        = "notification_api_key"
+  description = "Notification API key"
+  tags        = local.default_tags
+}
+
+resource "aws_secretsmanager_secret" "os_places_api_key" {
+  name        = "os_places_api_key"
+  description = "OS Places API key"
+  tags        = local.default_tags
+}
+
+resource "aws_secretsmanager_secret" "symfony_app_secret" {
+  name        = "symfony_app_secret"
+  description = "Symfony application secret"
+  tags        = local.default_tags
+}
+
+resource "aws_secretsmanager_secret" "database_password" {
+  name        = "database_password"
+  description = "Database password"
+  tags        = local.default_tags
+}
+
+# TODO Remove Import Blocks
+data "aws_secretsmanager_secret" "public_api_password" {
+  name = "sirius_public_api_password"
+}
+
+import {
+  to = aws_secretsmanager_secret.public_api_password
+  id = data.aws_secretsmanager_secret.public_api_password.arn
+}
+
+data "aws_secretsmanager_secret" "notification_api_key" {
+  name = "notification_api_key"
+}
+
+import {
+  to = aws_secretsmanager_secret.notification_api_key
+  id = data.aws_secretsmanager_secret.notification_api_key.arn
+}
+
+data "aws_secretsmanager_secret" "os_places_api_key" {
+  name = "os_places_api_key"
+}
+
+import {
+  to = aws_secretsmanager_secret.os_places_api_key
+  id = data.aws_secretsmanager_secret.os_places_api_key.arn
+}
+
+data "aws_secretsmanager_secret" "symfony_app_secret" {
+  name = "symfony_app_secret"
+}
+
+import {
+  to = aws_secretsmanager_secret.symfony_app_secret
+  id = data.aws_secretsmanager_secret.symfony_app_secret.arn
+}
+
+data "aws_secretsmanager_secret" "database_password" {
+  name = "database_password"
+}
+
+import {
+  to = aws_secretsmanager_secret.database_password
+  id = data.aws_secretsmanager_secret.database_password.arn
+}
